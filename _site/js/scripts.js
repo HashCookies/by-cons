2b553c6d4d606e28eda0317b93a729ff72564832
Milind Alvares <milind@hashcooki.es>
2017-11-13 15:24:44 +0530

    Add files

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -2,6 +2,7 @@
 
 var winH = $(window).height();
 var winW = $(window).width();
+console.log(winW, winH);
 
 $(document).ready(function() {
    var isMobile = window.matchMedia("only screen and (max-width: 760px)");
@@ -11,24 +12,6 @@ $(document).ready(function() {
      devW = 'auto';
      devH = 'auto';
    }
-   $('#main-cover').height(winH).width(winW);
-   $('.flexslider, .slide').height(devH).width(devW);
+   $('.cover').outerHeight(winH).width(winW);
 
-  $('.flexslider').each(function() {
-    $(this).flexslider({
-      animation: "fade",
-      animationLoop: false,
-      itemWidth: winW,
-      maxItems: 1
-    });
-  });
-
-  $('#sign-petition').on('click', function() {
-    $(document).scrollTo($('.petition-form'), 600, {easing: 'linear'});
-    return false;
-  });
-  $('#main-link').on('click', function() {
-    $(document).scrollTo($('#intro-section'), 600, {easing: 'linear'});
-    return false;
-  });
 });
